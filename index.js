4d20946c62abc6b565e4edf2b4d782fbf232b6bf
ruchitaghodasara <ruchighodasara16@gmail.com>
2022-04-28 16:01:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -27,3 +27,31 @@ const mernStack = ['MongoDB', 'Express', 'React', 'Node'];
 for (let i = 0; i < countries.length; i++) {
   console.log(countries[i]);
 }
+let resultE = [],
+  resultO = [];
+for (let i = 0; i < 100; i++) {
+  if (i % 2 == 0) {
+    resultE.push(i);
+  }
+}
+for (let i = 0; i < 100; i++) {
+  if (i % 2 !== 0) {
+    resultO.push(i);
+  }
+}
+console.log(resultE);
+console.log(resultO);
+function isPrime(num) {
+  if (num < 2) return false;
+  for (var i = 2; i < num; i++) {
+    if (num % i == 0) return false;
+  }
+  return num;
+}
+let result = [];
+for (let i = 0; i < 100; i++) {
+  if (isPrime(i)) {
+    result.push(i);
+  }
+}
+console.log(result);
