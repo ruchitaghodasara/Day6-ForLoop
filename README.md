ad1fef842e10b0e562a81a40e13d24c212011c8b
ruchitaghodasara <ruchighodasara16@gmail.com>
2022-04-28 14:09:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fatxko
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fatxko)
---
+++ index.js
@@ -0,0 +1,25 @@
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+];
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node'];
